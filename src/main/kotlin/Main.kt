3e45608bc2d294f8e120e14b387ffe5e698fefe5
leminhan <leminhan@amazon.com>
2024-08-08 16:59:34 -0500

    Respong to ping

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,13 @@ fun main(args: Array<String>) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.err.println("Logs from your program will appear here!")
 
-     var serverSocket = ServerSocket(6379)
+    var serverSocket = ServerSocket(6379)
 
-     // Since the tester restarts your program quite often, setting SO_REUSEADDR
-     // ensures that we don't run into 'Address already in use' errors
-     serverSocket.reuseAddress = true
+    // Since the tester restarts your program quite often, setting SO_REUSEADDR
+    // ensures that we don't run into 'Address already in use' errors
+    serverSocket.reuseAddress = true
 
-     serverSocket.accept() // Wait for connection from client.
-     println("accepted new connection")
+    val client = serverSocket.accept()
+    val outputClient = client.getOutputStream()
+    outputClient.write("+PONG\r\n".toByteArray())
 }
